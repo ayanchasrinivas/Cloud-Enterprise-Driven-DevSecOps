e17cc468096b813305cf2785186c428e1ba09ef7
ayanchasrinivas <ayanchasrinivas001@gmail.com>
2026-07-21 00:17:39 +0530

    Added Trivy Dependency libs installation

--- vulnerable-app/src/main/java/com/devsecops/lab/vulnerable_app/controller/SQLInjectionController.java
+++ vulnerable-app/src/main/java/com/devsecops/lab/vulnerable_app/controller/SQLInjectionController.java
@@ -14,23 +14,19 @@ public class SQLInjectionController {
     @GetMapping("/user")
     public String findUser(@RequestParam String username) throws Exception {
 
-        // Connection connection = DriverManager.getConnection(
-        //         "jdbc:mysql://localhost:3306/devsecops",
-        //         "root",
-        //         "password"
-        // );
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/devsecops",
+                "root",
+                "password"
+        );
 
-        // Statement statement = connection.createStatement();
+        Statement statement = connection.createStatement();
 
-        // String query =
-        //         "SELECT * FROM users WHERE username = '" + username + "'";
+        String query =
+                "SELECT * FROM users WHERE username = '" + username + "'";
 
-        // String sql =
-        //     "SELECT * FROM users WHERE username='"
-        //         + username
-        //         + " ' ";
 
-        // ResultSet resultSet = statement.executeQuery(query);
+        ResultSet resultSet = statement.executeQuery(query);
 
         return query;
     }
